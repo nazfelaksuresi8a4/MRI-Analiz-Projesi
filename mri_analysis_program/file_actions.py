f60ad8aa56cd8b1c7248d59c3757e00f23b46909
Aslan akbey <agmroc@gmail.com>
2025-10-28 09:58:13 +0300

    Refactor matrix_returner for better file handling
    Refactor matrix_returner function to handle .nii and .nii.gz files more efficiently, with improved error handling and data retrieval.

--- mri_analysis_program/file_actions.py
+++ mri_analysis_program/file_actions.py
@@ -2,33 +2,58 @@ import nibabel as nib
 import pydicom as pydcm
 
 def matrix_returner(path,name):
-    if path:
-        if type(name) == type('str') or type(name) == str:
-            if str(path).endswith('.nii'):
+    if str(path).endswith('.nii'):
+        if type(name) == type('str') or type(name) == type('str'):
                 try:
-                    string_path = str(name)
+                    string_path = str(path)
+
+                    nib_file = nib.load(string_path)
+
+                    fdata = nib_file.get_data()
+                    
+                    h,w,d = fdata.shape
+
+                    return fdata,h,w,d
+                except Exception as matrix_exception:
+                    try:
+                        string_path = str(path)
+
+                        nib_file = nib.load(string_path)
+
+                        fdata = nib_file.get_data()
+                        
+                        h,w,d = fdata.shape
+
+                        return fdata,h,w,d
+                    except:
+                        print(matrix_exception,'m1-file_actions')
+
+    elif str(path).endswith('.nii.gz'):
+        if type(path) == type('str') or type(path) == type('str'):
+                try:
+                    string_path = str(path)
 
                     nib_file = nib.load(string_path)
 
                     fdata = nib_file.get_fdata()
-                    h,w,d = fdata.shape
 
-                    return (fdata,h,w,d)
+                    fdata.astype('float32')
+
+                    metashape = fdata.shape
+                    if len(metashape) == 3:
+                        h,w,d = metashape
+
+                        print(fdata,h,w,d)
+                        return (fdata,h,w,d) 
+
+                    elif len(metashape) == 4:
+                        h1,h2,w,d = metashape
+
+                        print(fdata,h1,h2,w,d)
+                        return (fdata,h1,h2,w,d)
+
                 except Exception as matrix_exception:
-                    if path:
-                        if type(path) == type('str') or type(path) == str:
-                            if str(path).endswith('.nii'):
-                                try:
-                                    string_path = str(path)
-
-                                    nib_file = nib.load(string_path)
-
-                                    fdata = nib_file.get_fdata()
-                                    h,w,d = fdata.shape
-
-                                    return (fdata,h,w,d)
-                                except Exception as matrix_exception:
-                                    print(matrix_exception)
-                                    pass
+                    print(matrix_exception)
+                    print('MRI-matrix-returner-2')
 
                 
